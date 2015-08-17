1c7891da92fce0e2c5e73cece82065334fb565f4
Christopher Sumnicht <csumnicht@berkeley.edu>
2015-08-16 21:48:46 -0700

    Added requires to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,17 @@ var source = require('vinyl-source-stream');
 var browserify = require('browserify');
 var watchify = require('watchify');
 var reactify = require('reactify');
+var gulpif = require('gulp-if');
+var uglify = require('gulp-uglify');
+var streamify = require('gulp-streamify');
+var notify = require('gulp-notify');
+var concat = require('gulp-concat');
+var cssmin = require('gulp-cssmin');
+var gutil = require('gulp-util');
+var shell = require('gulp-shell');
+var glob = require('glob');
+var livereload = require('gulp-livereload');
+var jasminePhantomJs = require('gulp-jasmine2-phantomjs');
 
 gulp.task('browserify', function() {
     var bundler = browserify({
