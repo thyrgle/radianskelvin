02c4660f60aaacef0d8f7fee2fc196cfb7e46177
Christopher Sumnicht <csumnicht@berkeley.edu>
2015-08-17 17:46:31 -0700

    Added some boiler plate to app folder

---
+++ app/App.js
@@ -0,0 +1,10 @@
+/** @jsx React.DOM */
+var React = require('react');
+
+var App = React.createClass({
+    render: function() {
+        return (
+                <h1>Hello World!</h1>
+               );
+    }
+});
