333b56e3279a8939e934e4fe89bd988ea3e575d3
Christopher Sumnicht <csumnicht@berkeley.edu>
2015-08-18 22:18:34 -0700

    Fixed indentation

--- app/App.js
+++ app/App.js
@@ -2,15 +2,14 @@
 var React = require('react');
 
 var App = React.createClass({
-	render: function() {
-		return (
-                        <div>
-			        <h1>Radians Kelvin</h1>
-                                <p>Today it is:</p>
-                        </div>
-		);
-	}
-	
+    render: function() {
+        return (
+            <div>
+              <h1>Radians Kelvin</h1>
+              <p>Today it is:</p>
+            </div>
+        );
+    }	
 });
 	
 module.exports = App;
